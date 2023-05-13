b66537baf2fb3c39d72484915a173aa44de1fd91
DevJoao <joaovitorlucasdev@gmail.com>
2023-05-13 20:46:57 -0300

    Blocos de código

---
+++ primeiro_codigo.py
@@ -0,0 +1,15 @@
+import os
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+    
+print(msg)
