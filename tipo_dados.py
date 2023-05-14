1d31a5b79c1de021c4a13ac771e5e7619d314b77
DevJoao <joaovitorlucasdev@gmail.com>
2023-05-14 00:29:55 -0300

    Dados

---
+++ tipo_dados.py
@@ -0,0 +1,14 @@
+# Colocamos um valor no fichário usando a Atribuição
+# aproveitamos para colocar uma etiqueta escrita o identificador "letra"
+letra = "B"
+
+# Python teve que achar uma pasta vazia para armazenar nosso valor
+id(letra)
+139862029254896
+
+# Python precisou colocar o valor "100010" dentro de um envelope
+# neste envelope contém também instruções de como usar este valor para obter
+# a letra "B".
+# a função type() nos diz qual tipo de envelope foi usado
+type(letra)
+str
